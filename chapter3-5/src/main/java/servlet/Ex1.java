f3a03035afc2783283fb1f6448e09a81015849bf
codeaid-dev <codeaid.lab@gmail.com>
2025-01-10 21:23:18 +0900

    first commit

---
+++ chapter3-5/src/main/java/servlet/Ex1.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ex1")
+public class Ex1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h2>実習1</h2>");
+		out.println("<p>名前：" + (name != null ? name : "名無し") + "</p>");
+	}
+
+}
