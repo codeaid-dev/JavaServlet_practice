11b6ecc104b249ae6267f703dad152564c66d288
codeaid-dev <codeaid.lab@gmail.com>
2025-01-12 18:30:35 +0900

    fix add

--- chapter3-5/src/main/java/servlet/Ex4.java
+++ chapter3-5/src/main/java/servlet/Ex4.java
@@ -2,8 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,18 +9,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ex3")
-public class Ex3 extends HttpServlet {
+@WebServlet("/ex4")
+public class Ex4 extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
-    LocalDateTime today = LocalDateTime.now();
-    String result = today.format(dtf);
-    out.println("<h2>実習3</h2>");
-    out.println("<p>今日の日付(年月日)：" + result + "</p>");
+    String url = request.getRequestURL().toString();
+    String method = request.getMethod();
+    out.println("<h2>実習4</h2>");
+    out.println("<p>URL：" + url + "</p>");
+    out.println("<p>メソッド：" + method + "</p>");
   }
 
 }
