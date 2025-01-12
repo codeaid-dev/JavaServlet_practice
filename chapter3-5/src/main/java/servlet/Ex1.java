260be61fbb6be8e5f380fd9cc559915b6b5e9aae
codeaid-dev <codeaid.lab@gmail.com>
2025-01-12 12:39:39 +0900

    add

--- chapter3-5/src/main/java/servlet/Ex1.java
+++ chapter3-5/src/main/java/servlet/Ex1.java
@@ -11,14 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/ex1")
 public class Ex1 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		String name = request.getParameter("name");
-		out.println("<h2>実習1</h2>");
-		out.println("<p>名前：" + (name != null ? name : "名無し") + "</p>");
-	}
-
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html; charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    String name = request.getParameter("name");
+    out.println("<h2>実習1</h2>");
+    out.println("<p>名前：" + (name != null ? name : "名無し") + "</p>");
+  }
 }
---
+++ chapter3-5/src/main/java/servlet/Ex3.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ex3")
+public class Ex3 extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
+    LocalDateTime today = LocalDateTime.now();
+    String result = today.format(dtf);
+    out.println("<h2>実習3</h2>");
+    out.println("<p>今日の日付：" + result + "</p>");
+  }
+
+}
