a52edd26a57b031f16652d8a47f62582f0ac7570
codeaid-dev <codeaid.lab@gmail.com>
2025-12-09 14:57:19 +0900

    fix

--- chapter11to13/src/main/java/servlet/Ex6About.java
+++ chapter11to13/src/main/java/servlet/Ex6About.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ex6about")
+@WebServlet("/about")
 public class Ex6About extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
